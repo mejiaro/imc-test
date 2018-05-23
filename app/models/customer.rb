985baff689299c8349e58402d6e736ad1726bc3c
Ricardo Mejía <mejiaro@gmail.com>
2018-05-23 07:26:58 -0600

    regexp should only validate email

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,7 +3,9 @@ class Customer < ApplicationRecord
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
 
-	validates :email, :name, :company, presence: true, length: { maximum: 255 },
+	validates :email, presence: true, length: { maximum: 255 },
     	format: {with: VALID_EMAIL_REGEX},
     	uniqueness: {case_sensitive: false}
+
+  validates :name, :company, presence: true
 end
